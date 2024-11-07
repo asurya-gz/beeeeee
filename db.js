e2014edb0f0a36ad224df1a83a49320463015dfc
agsry <agungkdp208@gmail.com>
2024-11-07 17:48:44 +0700

    asas

--- db.js
+++ db.js
@@ -2,21 +2,21 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 // Buat koneksi ke database lokal
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
-
-// Buat koneksi database online
 // const db = mysql.createConnection({
 //   host: process.env.DB_HOST,
 //   user: process.env.DB_USER,
 //   password: process.env.DB_PASSWORD,
 //   database: process.env.DB_NAME,
-//   port: process.env.DB_PORT,
 // });
+
+// Buat koneksi database online
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
 
 // Hubungkan ke database
 db.connect((err) => {
